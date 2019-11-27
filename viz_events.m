16ac04816053b3f982a0bab002195ce678353285
Momchil Tomov <tomov90@gmail.com>
2019-11-26 19:15:37 -0500

    viz_events nice

--- viz_events.m
+++ viz_events.m
@@ -3,8 +3,10 @@
 % https://www.mathworks.com/help/database/ug/mongo.html#d117e86584
 conn = mongo('127.0.0.1', 27017, 'heroku_7lzprs54')
 
+subj_id = '26';
+
 % https://www.mathworks.com/help/database/ug/mongo.find.html
-runs = find(conn, 'runs', 'query', '{"subj_id": "26"}', 'limit', 10)
+runs = find(conn, 'runs', 'query', sprintf('{"subj_id": "%s"}', subj_id), 'limit', 10)
 
 
 figure;
@@ -13,38 +15,68 @@ for r = 1:length(runs)
     run = runs(r)
     blocks = run.blocks;
 
+    subplot(length(runs), 1, r);
+    hold on;
+
     dur = round(run.end_time - run.scan_start_time + 5) * 1000;
-    st = zeros(1,dur);
-    en = zeros(1,dur);
+    rr = zeros(1,dur);
+    bb = zeros(1,dur);
+    ii = zeros(1,dur);
+    pp = zeros(1,dur);
 
     offs = 1;
-    st(round(offs * 1000)) = 16;
-    en(round((run.end_time - run.scan_start_time + offs) * 1000)) = -16;
+    st = round(offs * 1000) ;
+    en = round((run.end_time - run.scan_start_time + offs) * 1000);
+    rr(st:en) = 8;
+    text((st*0.7+en*0.3), 7, sprintf('run %d', run.run_id), 'interpreter', 'none');
 
     x = -offs*1000:(dur - 1000*offs - 1);
 
     for b = 1:length(blocks)
         block = blocks(b);
         instances = block.instances;
-        
-        st(round((block.start_time - run.scan_start_time + offs) * 1000)) = 8;
-        en(round((block.end_time - run.scan_start_time + offs) * 1000)) = -8;
+       
+        st = round((block.start_time - run.scan_start_time + offs) * 1000) + 3;
+        en = round((block.end_time - run.scan_start_time + offs) * 1000) - 3;
+        bb(st:en) = 6;
+        text((st*0.7+en*0.3), 5, block.game.name, 'interpreter', 'none');
 
         for i = 1:length(instances)
             instance = instances(i);
 
-            st(round((instance.start_time - run.scan_start_time + offs) * 1000)) = 4;
-            en(round((instance.end_time - run.scan_start_time + offs) * 1000)) = -4;
+            st = round((instance.start_time - run.scan_start_time + offs) * 1000) + 5;
+            en = round((instance.end_time - run.scan_start_time + offs) * 1000) - 5;
+            ii(st:en) = 4;
+            text((st*0.7+en*0.3), 3, sprintf('level %d', instance.level_id), 'interpreter', 'none');
 
-            q = sprintf('{"subj_id": "26", "run_id": %d, "block_id": %d, "instance_id": %d}', r, b, i);
-            plays = find(conn, 'plays', 'query', q, 'limit', 10);
+            q = sprintf('{"subj_id": "26", "run_id": %d, "block_id": %d, "instance_id": %d}', run.run_id, block.block_id, instance.instance_id);
+            plays = find(conn, 'plays', 'query', q);
+
+            for p = 1:length(plays)
+                play = plays(p);
+
+                st = round((play.start_time - run.scan_start_time + offs) * 1000) + 7;
+                en = round((play.end_time - run.scan_start_time + offs) * 1000) - 7;
+                pp(st:en) = 2;
+                if isempty(play.win)
+                    win = 'T';
+                elseif play.win
+                    win = 'W';
+                else
+                    win = 'L';
+                end
+                text((st*0.9+en*0.1), 1, sprintf('%s', win), 'interpreter', 'none');
+            end
         end
     end
 
-    subplot(length(runs), 1, r);
-    plot(x, st, 'color', 'green');
-    hold on;
-    plot(x, en, 'color', 'red');
+    title(['Subject ', subj_id]);
+
+    plot(x, rr);
+    plot(x, bb);
+    plot(x, ii);
+    plot(x, pp);
+    legend({'run', 'blocks', 'instances', 'plays'});
 
 end
 
