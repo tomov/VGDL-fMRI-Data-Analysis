23d26014151c613318541f398637d961f14ec1cc
Momchil Tomov <tomov90@gmail.com>
2020-06-09 16:53:06 -0400

    neurosynth_rsa_HRR multiple ROIs

--- neurosynth_rsa_HRR.m
+++ neurosynth_rsa_HRR.m
@@ -5,11 +5,14 @@ use_smooth = false;
 glmodel = 1;
 nperms = 1000;
 
+filename = 'mat/neurosynth_rsa_HRR.mat';
 
 if use_smooth
     EXPT = vgdl_expt();
+    whole_brain_mask = ccnl_load_mask('masks/mask.nii');
 else
     EXPT = vgdl_expt_nosmooth();
+    whole_brain_mask = ccnl_load_mask('masks/mask_nosmooth.nii');
 end
 
 subjects = 1:length(EXPT.subject);
@@ -18,111 +21,155 @@ subjects = 1:length(EXPT.subject);
 % get ROIs
 %[roi_masks, region] = get_neurosynth_rois(lateralized);
 load('mat/get_neurosynth_rois_lat=true');
-mask = roi_masks{region == 12};
 
 load('mat/HRR_groundtruth_RDM_correlation.mat'); % game_names, mean_RDM
 game_names = cellfun(@strtrim, mat2cell(game_names, ones(size(game_names, 1), 1)), 'UniformOutput', false);
 
 behavioral_RDM = mean_RDM;
 
-upper = logical(triu(ones(size(neural_RDM)), 1));
-lower = logical(tril(ones(size(neural_RDM)), -1));
+upper = logical(triu(ones(size(behavioral_RDM)), 1));
+lower = logical(tril(ones(size(behavioral_RDM)), -1));
 
-clear S;
+%mask = roi_masks{region == 120};
+
+% precompute whole-brain patterns
+%
+clear U_all;
 
 for s = 1:length(subjects)
     subj = subjects(s);
-
-    clear U;
     for g = 1:length(game_names)
         game_name = game_names{g};
 
-        B = ccnl_get_beta_series(EXPT, glmodel, subj, game_name, mask);
+        B = ccnl_get_beta_series(EXPT, glmodel, subj, game_name, whole_brain_mask);
         assert(size(B,1) == 3);
-        B = B(:, ~any(isnan(B), 1));
+        %B = B(:, ~any(isnan(B), 1));
+        assert(~any(isnan(B(:))));
 
-        U(g,:) = B(2,:);
-        U(g + length(game_names),:) = B(3,:);
+        %{
+        % sanity
+        B1 = ccnl_get_beta_series(EXPT, glmodel, subj, game_name, mask);
+        B1 = B1(:, ~any(isnan(B1), 1));
+        B2 = B(:,mask(whole_brain_mask));
+        assert(immse(B1, B2) < 1e-10);
+        %}
+
+        U_all{s}(g,:) = B(2,:);
+        U_all{s}(g + length(game_names),:) = B(3,:);
     end
-
-    % RDM between partition 2 and partition 3
-    % 1) avoid comparing games in the same run
-    % 2) look at more asymptotic behavior (partition 1 is probs mostly learning)
-    tmp = squareRDMs(pdist(U, 'correlation'));
-    neural_RDM = tmp(1:length(game_names), length(game_names)+1:end);
-
-    % how symmetric is the RDM?
-    S(s).sym = how_sym(neural_RDM); % should be high
-
-    S(s).neural_RDM = neural_RDM;
-
-    % make it symmetric by averaging two halves
-    neural_RDM = 0.5 * (neural_RDM + neural_RDM'); 
-
-    % similarity of same vs different games
-    S(s).within_game = mean(diag(neural_RDM));
-    S(s).across_game = mean(neural_RDM(upper));
-    S(s).diff = S(s).across_game - S(s).within_game; % should be high
-
-    % rank correlation between behavioral and neural RDMs
-    S(s).rho = corr(neural_RDM(upper), behavioral_RDM(upper), 'type', 'Spearman'); % should be high
-
-    S(s).z_rho = atanh(S(s).rho);
-
-    % null distribution TODO dedupe
-    %
-    S(s).null_sym = nan(nperms, 1);
-    S(s).null_diff = nan(nperms, 1);
-    S(s).null_rho = nan(nperms, 1);
-
-    tic
-
-    for i = 1:nperms
-        U(1:length(game_names),:) = U(randperm(length(game_names)), :); % shuffle one half only
-
-        tmp = squareRDMs(pdist(U, 'correlation'));
-        neural_RDM = tmp(1:length(game_names), length(game_names)+1:end);
-
-        S(s).null_sym(i) = how_sym(neural_RDM); % should be high
-
-        neural_RDM = 0.5 * (neural_RDM + neural_RDM'); 
-
-        within_game = mean(diag(neural_RDM));
-        across_game = mean(neural_RDM(upper));
-        S(s).null_diff(i) = across_game - within_game; % should be high
-
-        S(s).null_rho(i) = corr(neural_RDM(upper), behavioral_RDM(upper), 'type', 'Spearman');
-    end
-
-    toc
-
 end
 
 
-sym = mean([S.sym]);
-diff = mean([S.diff]);
-rho = mean([S.rho]);
+clear ROI;
 
-null_sym = [mean([S.null_sym], 2); sym];
-null_diff = [mean([S.null_diff], 2); diff];
-null_rho = [mean([S.null_rho], 2); rho];
+for r = 1:length(roi_masks)
+    mask = roi_masks{r};
 
-p_sym = mean(sym < null_sym);
-p_diff = mean(diff < null_diff);
-p_rho = mean(rho < null_rho);
+    clear S;
+
+    tic
+
+    for s = 1:length(subjects)
+        subj = subjects(s);
+
+        % subset whole-brain patterns
+        U = U_all{s}(:, whole_brain_mask(mask));
+
+        % RDM between partition 2 and partition 3
+        % 1) avoid comparing games in the same run
+        % 2) look at more asymptotic behavior (partition 1 is probs mostly learning)
+        neural_RDM = 1 - corr(U(1:length(game_names),:)', U(length(game_names)+1:end,:)'); %-- alternative; not much faster
+
+        %{
+        % sanity -- note corr inputs are transposed compared to pdist 
+        tmp = squareRDMs(pdist(U, 'correlation'));
+        neural_RDM1 = tmp(1:length(game_names), length(game_names)+1:end);
+        assert(immse(neural_RDM1, neural_RDM) < 1e-10);
+        %}
+
+        % how symmetric is the RDM?
+        S(s).sym = how_sym(neural_RDM); % should be high
+
+        S(s).neural_RDM = neural_RDM;
+
+        % make it symmetric by averaging two halves
+        neural_RDM = 0.5 * (neural_RDM + neural_RDM'); 
+
+        % similarity of same vs different games
+        S(s).within_game = mean(diag(neural_RDM));
+        S(s).across_game = mean(neural_RDM(upper));
+        S(s).diff = S(s).across_game - S(s).within_game; % should be high
+
+        % rank correlation between behavioral and neural RDMs
+        S(s).rho = corr(neural_RDM(upper), behavioral_RDM(upper), 'type', 'Spearman'); % should be high
+
+        S(s).z_rho = atanh(S(s).rho);
+
+        % null distribution TODO dedupe
+        %
+        S(s).null_sym = nan(nperms, 1);
+        S(s).null_diff = nan(nperms, 1);
+        S(s).null_rho = nan(nperms, 1);
+
+        for i = 1:nperms
+            U(1:length(game_names),:) = U(randperm(length(game_names)), :); % shuffle one half only
+
+            neural_RDM = 1 - corr(U(1:length(game_names),:)', U(length(game_names)+1:end,:)');
+
+            S(s).null_sym(i) = how_sym(neural_RDM); % should be high
+
+            neural_RDM = 0.5 * (neural_RDM + neural_RDM'); 
+
+            within_game = mean(diag(neural_RDM));
+            across_game = mean(neural_RDM(upper));
+            S(s).null_diff(i) = across_game - within_game; % should be high
+
+            S(s).null_rho(i) = corr(neural_RDM(upper), behavioral_RDM(upper), 'type', 'Spearman');
+        end
+
+    end
+
+    ROI(r).S = S;
+
+    % avg across subjects
+    sym = mean([S.sym]);
+    diff = mean([S.diff]);
+    rho = mean([S.rho]);
+
+    % avg across subjects
+    null_sym = [mean([S.null_sym], 2); sym];
+    null_diff = [mean([S.null_diff], 2); diff];
+    null_rho = [mean([S.null_rho], 2); rho];
+
+    % note the importance of comparing aggregate statistics to aggregate statistics
+    % it would be incorrect e.g. to generate a null of individual subject symmetries 
+    % and then compare the average symmetry to it
+    ROI(r).p_sym = mean(sym < null_sym);
+    ROI(r).p_diff = mean(diff < null_diff);
+    ROI(r).p_rho = mean(rho < null_rho);
+
+    ROI(r).z_sym = (sym - mean(null_sym)) / std(null_sym);
+    ROI(r).z_diff = (diff - mean(null_diff)) / std(null_diff);
+    ROI(r).z_rho = (rho - mean(null_rho)) / std(null_rho);
+
+    ROI(r).sym = sym;
+    ROI(r).diff = diff;
+    ROI(r).rho = rho;
+
+    toc
+end
+
+    %{
+    sem = @(x) std(x) / sqrt(length(x));
+
+    m = [mean(sym) mean(diff) mean(z_rho)];
+    se = [sem(sym) sem(diff) sem(z_rho)];
+    bar([1 2 3], m);
+    hold on;
+    er = errorbar([1 2 3], m, se, 'linestyle', 'none');
+    %}
 
 figure;
-
-%{
-sem = @(x) std(x) / sqrt(length(x));
-
-m = [mean(sym) mean(diff) mean(z_rho)];
-se = [sem(sym) sem(diff) sem(z_rho)];
-bar([1 2 3], m);
-hold on;
-er = errorbar([1 2 3], m, se, 'linestyle', 'none');
-%}
-
 
 subplot(1,3,1);
 
@@ -156,3 +203,4 @@ set(gca, 'ytick', []);
 title('HRR RSA match');
 xlabel('Spearman $\rho$', 'interpreter', 'latex');
 
+save(filename, '-v7.3');
