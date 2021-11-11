afc6e14a83ab583eb22da10f5ec4287357ad76ba
Momchil Tomov <mtomov13@fasselogin01.rc.fas.harvard.edu>
2021-11-11 13:51:04 -0500

    ranksum -> signrank omg ... needs to be a paired test

--- plot_gp_CV_rois.m
+++ plot_gp_CV_rois.m
@@ -5,8 +5,10 @@ close all;
 fasse_ncf = false;
 %agg_filename = fullfile(get_mat_dir(fasse_ncf), 'gp_CV_rois_alpha=0.010_atlas=HarvardOxford-maxprob-thr0.mat');
 %agg_filename = fullfile(get_mat_dir(fasse_ncf), 'gp_CV_rois_alpha=0.010_atlas=AAL3v1.mat');
-%agg_filename = fullfile(get_mat_dir(fasse_ncf), 'gp_CV_rois_alpha=0.010_atlas=AAL2.mat'); % !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
-agg_filename = fullfile(get_mat_dir(fasse_ncf), 'gp_CV_rois_alpha=0.010_atlas=AAL2_grouped.mat');
+
+agg_filename = fullfile(get_mat_dir(fasse_ncf), 'gp_CV_rois_alpha=0.010_atlas=AAL2.mat'); % !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+%agg_filename = fullfile(get_mat_dir(fasse_ncf), 'gp_CV_rois_alpha=0.010_atlas=AAL2_grouped.mat');
+
 %agg_filename = fullfile(get_mat_dir(fasse_ncf), 'gp_CV_rois_alpha=0.010.mat');
 %agg_filename = fullfile(get_mat_dir(fasse_ncf), 'gp_CV_rois_alpha=0.001.mat');
 agg_filename
@@ -31,7 +33,8 @@ ylabel('\Delta BIC');
 
 figure('position', [673 90 1519 849]);
 ix = ismember(regressor_names, {'theory', 'DQN', 'PCA'});
-h = plot_gp_CV_rois_helper(fs(:,ix,:), 'ranksum', 'median', regressor_names(ix), roi_names, alpha);
+h = plot_gp_CV_rois_helper(fs(:,ix,:), 'signrank', 'median', regressor_names(ix), roi_names);
+%h = plot_gp_CV_rois_helper(fs(:,ix,:), 'signrank', 'median', regressor_names(ix), roi_names, alpha);
 %plot_gp_CV_rois_helper(fs(:,ix,:), 'ttest', 'mean', regressor_names(ix), roi_names);
 %ylim([0 0.1]);
 title('Fraction significant voxels in ROIs');
@@ -42,14 +45,14 @@ ylabel('Fraction significant voxels');
 figure('position', [73 90 1519 849]);
 ix = ismember(regressor_names, {'theory', 'sprite', 'interaction', 'termination'});
 cmap = [1 0.8 0.6 0.4]' * h(1).FaceColor;
-plot_gp_CV_rois_helper(fs(:,ix,:), 'ranksum', 'median', regressor_names(ix), roi_names, alpha); %colormap(winter(3)));
+plot_gp_CV_rois_helper(fs(:,ix,:), 'signrank', 'median', regressor_names(ix), roi_names, alpha); %colormap(winter(3)));
 title('Fraction significant voxels in ROIs');
 ylabel('Fraction significant voxels');
 
 figure('position', [73 90 1519 849]);
 ix = ismember(regressor_names, {'DQN', 'conv1', 'conv2', 'conv3', 'linear1', 'linear2'});
 cmap = [1 0.9 0.8 0.7 0.6 0.5]' * h(2).FaceColor;
-h = plot_gp_CV_rois_helper(fs(:,ix,:), 'ranksum', 'median', regressor_names(ix), roi_names, alpha); %colormap(autumn(5)));
+h = plot_gp_CV_rois_helper(fs(:,ix,:), 'signrank', 'median', regressor_names(ix), roi_names, alpha); %colormap(autumn(5)));
 title('Fraction significant voxels in ROIs');
 ylabel('Fraction significant voxels');
 %}
@@ -57,7 +60,7 @@ ylabel('Fraction significant voxels');
 
 %{
 figure;
-plot_gp_CV_rois_helper(fs, 'ranksum', regressor_names, roi_names);
+plot_gp_CV_rois_helper(fs, 'signrank', regressor_names, roi_names);
 title('Fraction significant voxels in ROIs');
 ylabel('Fraction significant voxels');
 %}
@@ -159,8 +162,8 @@ function h = plot_gp_CV_rois_helper(fs, test_type, statistic, regressor_names, r
                 switch test_type
                     case 'ttest'
                         [~,p,ci,stats] = ttest(y1, null_value);
-                    case 'ranksum'
-                        [p,~,stats] = signtest(y1, null_value, 'Tail','right');
+                    case 'signrank'
+                        [p,~,stats] = signrank(y1, null_value, 'Tail','right');
                 end
                 if p <= 0.05
                     text(x1, u_fs(m,r1) + 0.002, significance(p), 'HorizontalAlignment', 'center');
@@ -171,7 +174,10 @@ function h = plot_gp_CV_rois_helper(fs, test_type, statistic, regressor_names, r
 
         % paired
         %maxy = max(m_fs(m,:) + sem_fs(m,:));
-        maxy = max(u_fs(m,:))+ 0.003;
+        maxy = max(u_fs(m,:));
+        if exist('null_value') && ~isempty(null_value)
+            maxy = maxy + 0.003; % account for single tests
+        end
         for r1 = 1:nregressors
             for r2 = r1+1:nregressors
                 y1 = squeeze(fs(m,r1,:));
@@ -181,8 +187,8 @@ function h = plot_gp_CV_rois_helper(fs, test_type, statistic, regressor_names, r
                 switch test_type
                     case 'ttest'
                         [~,p,ci,stats] = ttest(y1,y2);
-                    case 'ranksum'
-                        [p,~,stats] = ranksum(y1, y2);
+                    case 'signrank'
+                        [p,~,stats] = signrank(y1, y2);
                 end
                 if p <= 0.05
                     plot([x1 x2], [maxy maxy] + 0.001, '-', 'color', [0 0 0]);
