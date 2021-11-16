2aaf4d00403e3f681d7bdc6db4e60fa26b172f76
Momchil Tomov <mtomov13@fasselogin01.rc.fas.harvard.edu>
2021-11-16 17:08:24 -0500

    plot_PETH_bars_AAL2_GP_EMPA_GLM_102_GP

--- show_figure.m
+++ show_figure.m
@@ -508,7 +508,74 @@ switch figure_name
             title(regions{m}, 'interpreter', 'none');
         end
 
- 
+
+    case 'plot_PETH_bars_AAL2_GP_EMPA_GLM_102_GP'
+        % plot_PETHs_bars.m
+
+        load(fullfile(get_mat_dir(false), 'PETHs_atlas=AAL2_GP_EMPA_GLM_102_GP.mat')); % !!!!!!!!!!!!
+        ROI_ix = 1:length(mask_filenames);
+
+        mask_filenames = mask_filenames(ROI_ix);
+        mask_name = mask_name(ROI_ix);
+        regions = regions(ROI_ix);
+        activations = activations(ROI_ix);
+
+        % optionally plot theory change flag only
+        %fields(find(strcmp(fields, 'theory_change_flag'))) = [];
+        fields(find(strcmp(fields, 'sprite_change_flag'))) = [];
+        fields(find(strcmp(fields, 'interaction_change_flag'))) = [];
+        fields(find(strcmp(fields, 'termination_change_flag'))) = [];
+        fields(find(strcmp(fields, 'block_start'))) = [];
+        fields(find(strcmp(fields, 'block_end'))) = [];
+        fields(find(strcmp(fields, 'instance_start'))) = [];
+        fields(find(strcmp(fields, 'instance_end'))) = [];
+
+        subjs = 1:1:32;
+
+        nROIs = length(mask_filenames);
+        nregressors = length(fields);
+        nsubjects = length(subjs);
+
+        as = nan(nROIs,nregressors,nsubjects);
+
+        cmap = colormap(jet(length(fields)));
+        t = PETH_dTRs * EXPT.TR; % s
+
+        % loop over masks
+        for m = 1:nROIs
+            disp(mask_name{m});
+
+            for i = 1:nregressors
+                field = fields{i};
+                disp(field)
+
+                D = activations(m).(field)(subjs,:); % subj x TRs PETH's
+                as(m,i,:) = mean(D(:, PETH_dTRs > 0), 2); % average across time, ignoring baseline
+            end
+        end
+
+        % Piggyback off of plot_gp_CV_rois.m
+        figure('pos', [49 329 2143 610]);
+
+        ix = 1:nregressors;
+        h = plot_gp_CV_rois_helper(as(:,ix,:), 'ttest', 'mean', fields(ix), regions, 0, cmap, 5, 1);
+        if exist('what', 'var') && strcmp(what, 'GP')
+            title('Average Fisher z-transformed Pearson correlation change in ROIs');
+            ylabel('\Delta z');
+        else
+            title('Average BOLD change in ROIs');
+            ylabel('\Delta BOLD');
+        end
+
+        % Prettyfy it 
+        % specifically for agg_filename = fullfile(get_mat_dir(fasse_ncf), 'gp_CV_rois_alpha=0.010_atlas=AAL2_GP_EMPA.mat');
+        text(1.5, 0.25, 'Frontal/Motor', 'fontsize', 12, 'HorizontalAlignment', 'center');
+        plot([2.5 2.5], [0 0.3], '--', 'color', [0.5 0.5 0.5]);
+        text(3.5, 0.25, 'Dorsal/Parietal', 'fontsize', 12, 'HorizontalAlignment', 'center');
+        plot([4.5 4.5], [0 0.3], '--', 'color', [0.5 0.5 0.5]);
+        text(6, 0.25, 'Ventral/Temporal', 'fontsize', 12, 'HorizontalAlignment', 'center');
+        legend(fields(ix), 'interpreter', 'none');
+         
 
     otherwise
         assert(false, 'Invalid figure name');
