a07cd45a161909b342208f9b509d92f0160a4244
Momchil Tomov <mtomov13@fasselogin01.rc.fas.harvard.edu>
2022-04-03 10:43:29 -0400

    VAE_e1k

--- load_VAE_kernel.m
+++ load_VAE_kernel.m
@@ -1,5 +1,5 @@
 function [ker] = load_VAE_kernel(subj_id, which_run_ids, normalize)
-    filename = sprintf('VAE_subject_kernel_subj=%d_sigma_w=1.000_norm=%d.mat', subj_id, normalize);
+    filename = sprintf('VAE_e1k_subject_kernel_subj=%d_sigma_w=1.000_norm=%d.mat', subj_id, normalize);
     filename = fullfile(get_mat_dir(), filename);
     filename
     load(filename, 'embedding_kernel', 'r_id');
